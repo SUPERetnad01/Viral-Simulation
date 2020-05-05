7e1f870e3281715c72a9e3efefc8bb0fbb546744
dante <danteklijn01@student.nhlstenden.com>
2020-05-05 10:29:33 +0200

    :sparkles: Correct working A

---
+++ MovmentStrategy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dante Klijn on 05/05/2020.
+//
+
+#include "MovmentStrategy.h"
---
+++ MovmentStrategy.h
@@ -0,0 +1,16 @@
+//
+// Created by Dante Klijn on 05/05/2020.
+//
+
+#ifndef VIRAL_SIMULATION_MOVMENTSTRATEGY_H
+#define VIRAL_SIMULATION_MOVMENTSTRATEGY_H
+
+
+
+class MovmentStrategy {
+
+};
+
+
+
+#endif //VIRAL_SIMULATION_MOVMENTSTRATEGY_H
